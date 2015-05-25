7621a01157f600c7ba1457424a3c23543ec61cb4
Alexis Algoud <alexis@atm-consulting.fr>
2015-05-25 10:52:51 +0200

    header

--- workstation.php
+++ workstation.php
@@ -203,6 +203,8 @@ function _liste_link(&$PDOdb, $fk_product) {
 		$titre=$langs->trans("CardProduct".$product->type);
 		$picto=($product->type==1?'service':'product');
 		dol_fiche_head($head, 'tabOF1', $titre, 0, $picto);
+        
+        headerProduct($product);
 	}
 	
 	$form=new TFormCore('auto','formLWS');
@@ -437,4 +439,51 @@ function _liste(&$PDOdb) {
 	
 	));
 	
+}
+
+
+function headerProduct(&$object) {
+   global $langs, $conf, $db; 
+    
+    $form = new Form($db);
+        
+    print '<table class="border" width="100%">';
+    
+    
+    // Ref
+    print '<tr>';
+    print '<td width="15%">' . $langs->trans("Ref") . '</td><td colspan="2">';
+    print $form->showrefnav($object, 'ref', '', 1, 'ref');
+    print '</td>';
+    print '</tr>';
+    
+    // Label
+    print '<tr><td>' . $langs->trans("Label") . '</td><td>' . $object->libelle . '</td>';
+    
+    $isphoto = $object->is_photo_available($conf->product->multidir_output [$object->entity]);
+    
+    $nblignes = 5;
+    if ($isphoto) {
+        // Photo
+        print '<td valign="middle" align="center" width="30%" rowspan="' . $nblignes . '">';
+        print $object->show_photos($conf->product->multidir_output [$object->entity], 1, 1, 0, 0, 0, 80);
+        print '</td>';
+    }
+    
+    print '</tr>';
+    
+    
+    // Status (to sell)
+    print '<tr><td>' . $langs->trans("Status") . ' (' . $langs->trans("Sell") . ')</td><td>';
+    print $object->getLibStatut(2, 0);
+    print '</td></tr>';
+    
+    print "</table>\n";
+    
+  echo '<br />';
+        
+   
+       
+        
+    
 }
