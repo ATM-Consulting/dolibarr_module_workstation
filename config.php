1f7f6eaed2385216acd93183020b810355c3884e
Alexis Algoud <alexis@atm-consulting.fr>
2015-06-15 14:26:52 +0200

    FIX inclusion

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-	require('config.default.php');
+	require(__DIR__.'/config.default.php');
 
     dol_include_once('/workstation/class/workstation.class.php');
     dol_include_once('/workstation/lib/workstation.lib.php');    
