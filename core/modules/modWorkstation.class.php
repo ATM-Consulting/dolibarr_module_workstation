d1e764ad4dc6c62489b7b739b2e2fa195ac56500
phf <phf@atm-consulting.fr>
2017-04-12 14:29:55 +0200

    Add editor name

--- core/modules/modWorkstation.class.php
+++ core/modules/modWorkstation.class.php
@@ -44,6 +44,7 @@ class modWorkstation extends DolibarrModules
 
         $this->db = $db;
 
+		$this->editor_name = 'ATM-Consulting';
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
 		$this->numero = 104320; // 104000 to 104999 for ATM CONSULTING
